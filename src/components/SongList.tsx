9082918bcfbc0dfa4acef5e6c9651eb56c1b235b
ohayonb <ohayonzzb@gmail.com>
2023-09-06 10:17:02 +0300

    handle error when cant getting songs

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -3,55 +3,66 @@ import { Song } from "../models/Song"
 import { songService } from "../services/song.service"
 
 export const SongList = () => {
-    const [songs, setSongs] = useState<Song[]>([])
-    const [sortedSongs, setSortedSongs] = useState<Song[]>([]);
-    const [sortOrder, setSortOrder] = useState('asc');
+  const [songs, setSongs] = useState<Song[]>([])
+  const [sortedSongs, setSortedSongs] = useState<Song[]>([]);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [error, setError] = useState<string | null>(null)
 
-    useEffect(() => {
-        try {
-            songService.getAll().then(songs => {
-                setSongs(songs)
-            })
-        } catch (err) {
-            console.error('Error getting songs', err)
-        }
-    }, [])
+  useEffect(() => {
+    setError(null)
 
-    useEffect(() => {
-        const sortedSongs = songs.slice().sort((a, b) => {
-          if (sortOrder === 'asc') {
-            return a.band.localeCompare(b.band);
-          } else {
-            return b.band.localeCompare(a.band);
-          }
-        });
-        setSortedSongs(sortedSongs);
-      }, [songs, sortOrder]);
+    const fetchSongs = async () => {
+      try {
+        const songs = await songService.getAll()
+        setSongs(songs)
+      } catch (err) {
+        console.error('Error getting songs', err)
+        setError('An error occurred while getting songs. Please try again.');
+      }
+    };
 
-    const toggleSortOrder = () => {
-        setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
-      };
+    fetchSongs();
+  }, []);
+
+  useEffect(() => {
+    const sortedSongs = songs.slice().sort((a, b) => {
+      if (sortOrder === 'asc') {
+        return a.band.localeCompare(b.band);
+      } else {
+        return b.band.localeCompare(a.band);
+      }
+    });
+    setSortedSongs(sortedSongs);
+  }, [songs, sortOrder]);
+
+  const toggleSortOrder = () => {
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+  };
 
   return (
     <section className="song-list">
+      {error ? (
+        <div className="error-message">{error}</div>
+      ) : (
         <table className="song-table">
-        <thead>
-          <tr>
-            <th className="table-order" onClick={toggleSortOrder}>Band</th>
-            <th>Title</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortedSongs.map((song, index) => (
-            <tr key={index}>
-              <td>{song.band}</td>
-              <td>{song.songName}</td>
-              <td>{song.year}</td>
+          <thead>
+            <tr>
+              <th className="table-order" onClick={toggleSortOrder}>Band</th>
+              <th>Title</th>
+              <th>Year</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {sortedSongs.map((song, index) => (
+              <tr key={index}>
+                <td>{song.band}</td>
+                <td>{song.songName}</td>
+                <td>{song.year}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </section>
   )
 }
